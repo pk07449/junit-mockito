109b315dededa29bafe68573cd652d8ceeb4c84e
rashidi <rashidi@zin.my>
2014-04-09 17:37:00 +0800

    Apply static import for a nicer look.

--- src/test/java/com/timwe/sg/apac/techtalk/tdd/subscription/SubscriptionManagerImplTest.java
+++ src/test/java/com/timwe/sg/apac/techtalk/tdd/subscription/SubscriptionManagerImplTest.java
@@ -6,7 +6,8 @@ import com.timwe.sg.apac.techtalk.tdd.customer.CustomerManager;
 import com.timwe.sg.apac.techtalk.tdd.customer.CustomerStatusEnum;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author Rashidi Zin
@@ -32,24 +33,24 @@ public class SubscriptionManagerImplTest extends AbstractTest {
         $.customerManager = customerManager;
         $.repository = repository;
 
-        Mockito.when(subscription.getCustomer()).thenReturn(customer);
-        Mockito.when(customer.getId()).thenReturn(1L);
-        Mockito.when(customerManager.get(1L)).thenReturn(customer);
-        Mockito.when(customer.getStatus()).thenReturn(CustomerStatusEnum.ACTIVE);
+        when(subscription.getCustomer()).thenReturn(customer);
+        when(customer.getId()).thenReturn(1L);
+        when(customerManager.get(1L)).thenReturn(customer);
+        when(customer.getStatus()).thenReturn(CustomerStatusEnum.ACTIVE);
     }
 
     @Test
     public void testCreate() {
-        Mockito.when(repository.findByCustomer(customer)).thenReturn(null);
-        Mockito.when(repository.create(subscription)).thenReturn(1L);
+        when(repository.findByCustomer(customer)).thenReturn(null);
+        when(repository.create(subscription)).thenReturn(1L);
 
         $.create(subscription);
 
-        Mockito.verify(subscription, Mockito.times(1)).getCustomer();
-        Mockito.verify(customer, Mockito.times(1)).getId();
-        Mockito.verify(customerManager, Mockito.times(1)).get(Mockito.anyLong());
-        Mockito.verify(customer, Mockito.times(1)).getStatus();
-        Mockito.verify(repository, Mockito.times(1)).create(subscription);
+        verify(subscription, times(1)).getCustomer();
+        verify(customer, times(1)).getId();
+        verify(customerManager, times(1)).get(anyLong());
+        verify(customer, times(1)).getStatus();
+        verify(repository, times(1)).create(subscription);
     }
 
     @Test
@@ -57,7 +58,7 @@ public class SubscriptionManagerImplTest extends AbstractTest {
         expect.expect(SubscriptionException.class);
         expect.expectMessage("User has to be ACTIVE");
 
-        Mockito.when(customer.getStatus()).thenReturn(CustomerStatusEnum.INACTIVE);
+        when(customer.getStatus()).thenReturn(CustomerStatusEnum.INACTIVE);
 
         $.create(subscription);
     }
@@ -67,7 +68,7 @@ public class SubscriptionManagerImplTest extends AbstractTest {
         expect.expect(SubscriptionException.class);
         expect.expectMessage("Customer information is required");
 
-        Mockito.when(subscription.getCustomer()).thenReturn(null);
+        when(subscription.getCustomer()).thenReturn(null);
         $.create(subscription);
     }
 
@@ -76,8 +77,8 @@ public class SubscriptionManagerImplTest extends AbstractTest {
         expect.expect(SubscriptionException.class);
         expect.expectMessage("Customer is already an active subscriber");
 
-        Mockito.when(repository.findByCustomer(customer)).thenReturn(subscription);
-        Mockito.when(subscription.getStatus()).thenReturn(SubscriptionStatusEnum.ACTIVE);
+        when(repository.findByCustomer(customer)).thenReturn(subscription);
+        when(subscription.getStatus()).thenReturn(SubscriptionStatusEnum.ACTIVE);
 
         $.create(subscription);
     }
