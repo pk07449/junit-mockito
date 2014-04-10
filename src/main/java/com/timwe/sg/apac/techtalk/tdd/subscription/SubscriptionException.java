bd9e363a7d08c3688eb0f444fe7b0a71e1feb630
rashidi <rashidi@zin.my>
2014-04-10 16:18:43 +0800

    Remove useless constructor.

--- src/main/java/com/timwe/sg/apac/techtalk/tdd/subscription/SubscriptionException.java
+++ src/main/java/com/timwe/sg/apac/techtalk/tdd/subscription/SubscriptionException.java
@@ -5,19 +5,7 @@ package com.timwe.sg.apac.techtalk.tdd.subscription;
  */
 public class SubscriptionException extends RuntimeException {
 
-    public SubscriptionException() {
-        super();
-    }
-
     public SubscriptionException(String message) {
         super(message);
-    }
-
-    public SubscriptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public SubscriptionException(Throwable cause) {
-        super(cause);
     }
 }
