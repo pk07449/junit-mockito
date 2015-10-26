553cf5b6d428b706c5564939080dec632988a846
Mohd Rashidi Bin Mohd Zin <mohdrashidi.mohdzin@gfk.com>
2015-10-26 11:27:39 +0800

    Update junit to 4.12 and mockito to 1.10.19

--- src/test/java/com/timwe/sg/apac/techtalk/tdd/AbstractTest.java
+++ src/test/java/com/timwe/sg/apac/techtalk/tdd/AbstractTest.java
@@ -2,15 +2,17 @@ package com.timwe.sg.apac.techtalk.tdd;
 
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 /**
  * @author Rashidi Zin
  */
-@RunWith(MockitoJUnitRunner.class)
 public abstract class AbstractTest {
 
     @Rule
     public ExpectedException expect = ExpectedException.none();
+
+    @Rule
+    public MockitoRule mockito = MockitoJUnit.rule();
 }
